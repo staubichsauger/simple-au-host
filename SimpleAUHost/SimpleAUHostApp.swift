9a60093460e3ac0c46e18a5e735be7f5eb26c499
staubichsauger <49568580+staubichsauger@users.noreply.github.com>
2026-03-21 12:11:46 +0100

    taller windows

--- SimpleAUHost/SimpleAUHostApp.swift
+++ SimpleAUHost/SimpleAUHostApp.swift
@@ -5,8 +5,9 @@ struct SimpleAUHostApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(minWidth: 720, minHeight: 520)
+                .frame(minWidth: 720, minHeight: 680)
         }
+        .defaultSize(width: 720, height: 680)
         .windowResizability(.contentSize)
     }
 }
